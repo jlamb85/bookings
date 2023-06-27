a3974b13e1a1cb31b1c6be46349b5212010ab844
Jim Lamb <jlamb85@gmail.com>
2023-06-27 07:51:59 -0400

    section 70

---
+++ cmd/web/main.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+	"encoding/gob"
+	"fmt"
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/alexedwards/scs/v2"
+	"gitlab.com/jlamb85/bookings/internal/config"
+	"gitlab.com/jlamb85/bookings/internal/handlers"
+	"gitlab.com/jlamb85/bookings/internal/models"
+	"gitlab.com/jlamb85/bookings/internal/render"
+)
+
+const portNumber = ":8080"
+
+var app config.AppConfig
+var session *scs.SessionManager
+
+// main is the main function
+func main() {
+	// what am I going to put in the session
+	gob.Register(models.Reservation{})
+
+	// change this to true when in production
+	app.InProduction = false
+
+	// set up the session
+	session = scs.New()
+	session.Lifetime = 24 * time.Hour
+	session.Cookie.Persist = true
+	session.Cookie.SameSite = http.SameSiteLaxMode
+	session.Cookie.Secure = app.InProduction
+
+	app.Session = session
+
+	tc, err := render.CreateTemplateCache()
+	if err != nil {
+		log.Fatal("cannot create template cache")
+	}
+
+	app.TemplateCache = tc
+	app.UseCache = false
+
+	repo := handlers.NewRepo(&app)
+	handlers.NewHandlers(repo)
+
+	render.NewTemplates(&app)
+
+	fmt.Println(fmt.Sprintf("Staring application on port %s", portNumber))
+
+	srv := &http.Server{
+		Addr:    portNumber,
+		Handler: routes(&app),
+	}
+
+	err = srv.ListenAndServe()
+	if err != nil {
+		log.Fatal(err)
+	}
+}
---
+++ go.mod
@@ -0,0 +1,13 @@
+module gitlab.com/jlamb85/bookings
+
+go 1.20
+
+require (
+	github.com/alexedwards/scs v1.4.1 // indirect
+	github.com/alexedwards/scs/v2 v2.5.1 // indirect
+	github.com/asaskevich/govalidator v0.0.0-20230301143203-a9d515a09cc2 // indirect
+	github.com/go-chi/chi v1.5.4 // indirect
+	github.com/justinas/nosurf v1.1.1 // indirect
+	golang.org/x/crypto v0.10.0 // indirect
+	golang.org/x/sys v0.9.0 // indirect
+)
